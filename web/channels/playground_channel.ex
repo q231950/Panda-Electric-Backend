3cd4ebaa9de4d84357538bd96e1fce9fdca5c3f3
Martin Kim Dung-Pham <kim@elbedev.com>
2016-07-31 11:34:59 +0200

    receive messages

--- web/channels/playground_channel.ex
+++ web/channels/playground_channel.ex
@@ -1,10 +1,16 @@
 defmodule HelloPhoenix.PlaygroundChannel do
   use Phoenix.Channel
 
-  def join("playground:main", _message, socket) do
+  def join("playground:main", message, socket) do
+    IO.inspect  message
     {:ok, socket}
   end
   def join("playground:" <> _private_playground_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_in("new:msg", %{"body" => body}, socket) do
+    broadcast! socket, "new:msg", %{body: body}
+    {:noreply, socket}
   end
 end
