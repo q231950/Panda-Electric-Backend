c88d04dcf26d29992bb0ba0a3f5ec68d8f088dd3
Martin Kim Dung-Pham <kim@elbedev.com>
2016-10-30 20:19:47 +0100

    asscociated

--- web/channels/estimation_channel.ex
+++ web/channels/estimation_channel.ex
@@ -4,12 +4,34 @@ defmodule HelloPhoenix.EstimationChannel do
   alias HelloPhoenix.PandaSession
 
   def join("estimation:" <> session_id, %{"user" => user}, socket) do
+    IO.puts "[Estimation Channel] User joins: #{user}"
+    socket = assign(socket, :session_uuid, session_id)
+
+    IO.inspect socket.assigns
+    users = usersArray(socket.assigns[:users], [%{ user => "" }])
+
+    socket = assign(socket, :users, users)
+    IO.inspect socket.assigns[:users]
     {:ok, socket}
   end
 
-  def handle_in("new:estimate", %{"fibonacci" => fibonacci}, socket) do
-    IO.puts fibonacci
+  defp usersArray(currentUsers, newUsers) do
+    if is_nil(currentUsers) do
+      IO.puts "Users is nil, need to return new users"
+      newUsers
+    else
+      IO.puts "Users is available"
+      IO.inspect(currentUsers ++ newUsers)
+      currentUsers ++ newUsers
+    end
+  end
+
+  def handle_in("new:estimate", %{"fibonacci" => fibonacci, "user" => user_id}, socket) do
+    IO.puts "[Estimation Channel] New estimation(fibonacci) #{fibonacci} from user #{user_id} in session #{socket.assigns[:session_uuid]}"
+    IO.inspect socket.assigns
+
     broadcast! socket, "new:fibonacci_result", %{fibonacci: fibonacci}
+
     {:noreply, socket}
   end
   def handle_in("new:estimate", %{"tshirt" => size}, socket) do
